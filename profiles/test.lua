048e50bbcf610721719bef9ec26920d33f8ff69d
Bach Le <bach@bullno1.com>
2014-06-23 11:34:03 +0800

    Delete unused profile 'test'

--- profiles/test.lua
+++
@@ -1,8 +0,0 @@
-local extraPath="./src/?.lua;./src/?/init.lua"
-package.path=extraPath..';'..package.path
-
-require "luacov"
-assert=require "luassert"
-
-local glider = require "glider"
-glider.start{}
