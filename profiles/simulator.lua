1fcefeb999eed9639288c4b89ec9b39c88e7c964
Bach Le <bach@bullno1.com>
2014-07-07 12:30:27 +0800

    Automatically restart simulator on resouce or script changes

--- profiles/simulator.lua
+++ profiles/simulator.lua
@@ -6,3 +6,12 @@ MOAIEnvironment.setValue("documentDirectory", ".sandbox/document")
 MOAIEnvironment.setValue("cacheDirectory", ".sandbox/cache")
 MOAIEnvironment.setValue("appID", os.getenv("APP_ID"))
 MOAIEnvironment.setValue("devName", "simulator")
+
+local function restartOnModification(action, file)
+	if action == "modified" then
+		Titan.restart()
+	end
+end
+
+Titan.addWatch("src", restartOnModification)
+Titan.addWatch("assets", restartOnModification)
