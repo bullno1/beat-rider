8cdb5d73cb093780c0f5bfa54868a8e78efe7169
Bach Le <bach@bullno1.com>
2014-07-09 11:45:28 +0800

    Remove unused files

--- src/glider/ClientEventListener.lua
+++
@@ -1,7 +0,0 @@
-local Network = require "glider.Network"
-local Listener = require "glider.Listener"
-
-return Listener.makeListener(..., Network.clientEvents, {
-	{"ReceiveConnect", "connect", "onConnect"},
-	{"ReceiveDisconnect", "disconnect", "onDisconnect"}
-})
--- src/glider/ServerEventListener.lua
+++
@@ -1,7 +0,0 @@
-local Network = require "glider.Network"
-local Listener = require "glider.Listener"
-
-return Listener.makeListener(..., Network.serverEvents, {
-	{"ReceivePlayerJoin", "playerJoin", "onPlayerJoin"},
-	{"ReceivePlayerLeave", "playerLeave", "onPlayerLeave"}
-})
