8bb00b38e41fb78c06b0278bdfb34aa126ee69b5
Bach Le <bach@bullno1.com>
2014-07-14 12:00:23 +0800

    Make track narrower and more bumpy

--- src/DeveloperOptions.lua
+++ src/DeveloperOptions.lua
@@ -7,7 +7,7 @@ analysis.track.trend_smoothing_factor = 0.03
 analysis.track.wave_window_radius     = 15
 analysis.track.base_window_radius     = 30
 
-analysis.slope.data_smoothing_factor  = 0.01
+analysis.slope.data_smoothing_factor  = 0.007
 analysis.slope.trend_smoothing_factor = 0.01
 analysis.slope.window_radius          = 10
 
@@ -16,8 +16,8 @@ analysis.notes.cluster_max_length = 0.3
 analysis.notes.cluster_max_size   = 3
 
 ride.time_scale          = 1000
-ride.track_width         = 200
-ride.max_bump_height     = 100
+ride.track_width         = 130
+ride.max_bump_height     = 150
 ride.ship_height         = 15
 ride.max_elevation       = 15
 ride.max_speed_variation = 0.2
