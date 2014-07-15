4254bc5fc38a8a286ae45276c4736fe1570f93ac
Bach Le <bach@bullno1.com>
2014-07-15 15:08:46 +0800

    Make ship more wobbly by moving camera further away from ship

--- src/BeatRider.lua
+++ src/BeatRider.lua
@@ -4,12 +4,15 @@ return component(..., function()
 	depends "TrackBound"
 	depends "glider.Actor"
 
+	property "TrackOffset"
+
 	msg("onCreate", function(self, ent)
 		self.rideController = Entity.getByName("RideController")
+		ent:setTrackOffset(0)
 	end)
 
 	msg("update", function(self, ent)
 		local pos = self.rideController:getSongPos()
-		ent:setTrackPosition(pos)
+		ent:setTrackPosition(pos + ent:getTrackOffset())
 	end)
 end)
