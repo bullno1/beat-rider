b64b7ae4653949d5ef08940274a378d18d44b018
Bach Le <bach@bullno1.com>
2014-07-18 11:08:57 +0800

    Slight adjustment to track

--- src/DeveloperOptions.lua
+++ src/DeveloperOptions.lua
@@ -1,6 +1,6 @@
 analysis.hop_size = 512
 
-analysis.onset_detection.method = "kl"
+analysis.onset_detection.method = "hfc"
 
 analysis.track.data_smoothing_factor  = 0.03
 analysis.track.trend_smoothing_factor = 0.03
@@ -11,8 +11,8 @@ analysis.slope.data_smoothing_factor  = 0.007
 analysis.slope.trend_smoothing_factor = 0.01
 analysis.slope.window_radius          = 10
 
-analysis.notes.energy_threshold   = 0.07
-analysis.notes.cluster_max_length = 0.3
+analysis.notes.energy_threshold   = 0.1
+analysis.notes.cluster_max_length = 0.2
 analysis.notes.cluster_max_size   = 3
 
 ride.time_scale          = 2000
@@ -20,6 +20,6 @@ ride.track_width         = 130
 ride.max_bump_height     = 300
 ride.ship_height         = 15
 ride.max_elevation       = 15
-ride.note_speed          = 0.3
+ride.note_speed          = 0.5
 ride.update_distance     = 3
 ride.max_speed_variation = 0.8
