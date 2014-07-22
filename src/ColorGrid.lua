04495307049cb24d08d344787fc31834a3040253
Bach Le <bach@bullno1.com>
2014-07-22 19:01:41 +0800

    Add overfill warning

--- src/ColorGrid.lua
+++ src/ColorGrid.lua
@@ -8,10 +8,15 @@ return component(..., function()
 	property "LineWidth"
 
 	msg("onCreate", function(self, ent)
-		self.columns = {}
-		self.countDown = 1
+		self.pulse = 0 -- a value which goes back and forth in [0, 1], used for effects
+		self.pulseSpeed = 0.08 -- pulsing speed
+
+		self.countDown = 0 -- count down value until matched tiles are flushed from the grid
+		self.hasMatch = {} -- arrays of boolean which indicates whether a column has any matched tiles
+		self.columns = {} -- storage for tiles
 		for columnIndex = 1, 3 do
 			self.columns[columnIndex] = {}
+			self.hasMatch[columnIndex] = false
 		end
 	end)
 
@@ -23,6 +28,18 @@ return component(..., function()
 				flushGrid(self, ent)
 			end
 		end
+
+		local pulseSpeed = self.pulseSpeed
+		local pulse = self.pulse + pulseSpeed
+		if pulse >= 1 then
+			pulse = 1
+			self.pulseSpeed = -pulseSpeed
+		elseif pulse <= 0 then
+			pulse = 0
+			self.pulseSpeed = -pulseSpeed
+		end
+
+		self.pulse = pulse
 	end)
 
 	msg("onDraw", function(self, ent)
@@ -36,16 +53,20 @@ return component(..., function()
 		local tileHeight = (totalHeight - vGap * (numRows - 1)) / numRows
 		local columns = self.columns
 		local countDown = self.countDown
+		local columns = self.columns
 
 		for col = 0, 2 do
+			local column = columns[col + 1]
 			for row = 0, numRows - 1 do
+				-- Calculate bounds for tile
 				local xMin, yMin, xMax, yMax =
 					xMin + col * (tileWidth + hGap),
 					yMin + row * (tileHeight + vGap),
 					xMin + col * (tileWidth + hGap) + tileWidth,
 					yMin + row * (tileHeight + vGap) + tileHeight
 
-				local tile = columns[col + 1][row + 1]
+				-- Draw tile
+				local tile = column[row + 1]
 				if tile then
 					if tile.colored then
 						if tile.matched then
@@ -62,8 +83,13 @@ return component(..., function()
 					end
 				end
 
+				-- Draw border
 				MOAIGfxDevice.setPenWidth(ent:getLineWidth())
-				MOAIGfxDevice.setPenColor(1, 1, 1)
+				if #column == numRows and not self.hasMatch[col + 1] then
+					MOAIGfxDevice.setPenColor(1, 1, self.pulse)
+				else
+					MOAIGfxDevice.setPenColor(1, 1, 1)
+				end
 				MOAIDraw.drawRect(xMin, yMin, xMax, yMax)
 			end
 		end
@@ -134,14 +160,22 @@ return component(..., function()
 		local columns = self.columns
 		local matchFound = false
 
+		-- Reset old states
 		forEachTile(self, ent, clearMatch)
+		local hasMatch = self.hasMatch
+		for i = 1, 3 do
+			hasMatch[i] = false
+		end
 
+		-- Find matches
 		forEachTile(self, ent, function(tile, rowIndex, columnIndex)
 			if tile and not visited[tile] then
 				local cluster = findAdjacentColoredTiles(columns, tile, rowIndex, columnIndex, nil, visited)
 				if cluster and #cluster >= 3 then
 					for _, tile in ipairs(cluster) do
 						tile.matched = true
+						local column = cluster[tile][2]
+						hasMatch[column] = true
 					end
 					matchFound = true
 				end
@@ -162,6 +196,7 @@ return component(..., function()
 	function findAdjacentColoredTiles(columns, centerTile, rowIndex, columnIndex, cluster, visited)
 		if isColored(centerTile) and not visited[centerTile] then
 			cluster = cluster or {}
+			cluster[centerTile] = { rowIndex, columnIndex }
 			table.insert(cluster, centerTile)
 			visited[centerTile] = true
 
