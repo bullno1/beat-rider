e98434a7decc94a01dcf71c054917584180046f1
Bach Le <bach@bullno1.com>
2014-07-28 12:46:45 +0800

    Some tweakings

--- src/DeveloperOptions.lua
+++ src/DeveloperOptions.lua
@@ -1,7 +1,7 @@
 analysis.hop_size = 512
 
-analysis.onset_detection.method   = "hfc"
-analysis.onset_detection.hop_size = 128
+analysis.onset_detection.method   = "specdiff"
+analysis.onset_detection.hop_size = 256
 
 analysis.track.data_smoothing_factor  = 0.03
 analysis.track.trend_smoothing_factor = 0.03
@@ -12,13 +12,13 @@ analysis.slope.data_smoothing_factor  = 0.007
 analysis.slope.trend_smoothing_factor = 0.01
 analysis.slope.window_radius          = 10
 
-analysis.notes.energy_threshold = 0.2
+analysis.notes.energy_threshold = 0.15
 analysis.notes.cluster_max_gap  = 0.1
 analysis.notes.cluster_max_size = 5
 
-analysis.notes.same_column_threshold  = 0.1
+analysis.notes.same_column_threshold  = 0.15
 analysis.notes.close_column_threshold = 0.4
-analysis.notes.avoid_threshold        = 0.3
+analysis.notes.avoid_threshold        = 0.4
 
 ride.time_scale          = 2000
 ride.track_width         = 130
